b5fc61bde7e997b014c312024b28b122a90d0473
Harald Held <harald.held@gmail.com>
2021-12-25 23:16:14 +0100

    Finally getting the raw data.

--- cmd/ruuviLogger/main.go
+++ cmd/ruuviLogger/main.go
@@ -1,7 +1,70 @@
 package main
 
-import "log"
+import (
+    "log"
+
+    "github.com/godbus/dbus/v5"
+    ble "github.com/muka/go-bluetooth/api"
+    "github.com/muka/go-bluetooth/bluez/profile/adapter"
+)
+
+const (
+    basement             = "C9:73:96:D3:F9:A0"
+    outside              = "F2:03:48:17:D1:28"
+    livingRoom           = "C5:CE:2D:AA:D3:87"
+    ruuviManufacturerID  = 0x0499
+    manufacturerDataProp = "ManufacturerData"
+)
 
 func main() {
-	log.Println("hello, world")
+    btAdapter, err := ble.GetDefaultAdapter()
+    if err != nil {
+        log.Fatalf("could not find a Bluetooth adapter: %s", err)
+    }
+
+    discoveryOptions := adapter.DiscoveryFilter{
+        UUIDs:         []string{},
+        RSSI:          0,
+        Pathloss:      0,
+        Transport:     "le",
+        DuplicateData: true,
+    }
+
+    discovery, cancelDiscovery, err := ble.Discover(btAdapter, &discoveryOptions)
+    if err != nil {
+        log.Fatalf("error discovering Bluetooth devices: %s", err)
+    }
+    defer cancelDiscovery()
+
+    for device := range discovery {
+        log.Printf("found device: %+v", device.Path)
+
+        dev, err := btAdapter.GetDeviceByAddress(livingRoom)
+        if err != nil {
+            log.Printf("error getting device by address: %s", err)
+        }
+
+        if dev != nil {
+            propsCh, err := dev.WatchProperties()
+
+            if err == nil {
+                go func() {
+                    for {
+                        select {
+                        case msg := <-propsCh:
+                            if msg.Name == manufacturerDataProp {
+                                if values, ok := msg.Value.(map[uint16]dbus.Variant)[ruuviManufacturerID]; ok {
+                                    log.Printf("values: %x", values.Value().([]byte))
+                                }
+                            }
+                        }
+                    }
+                }()
+
+                break
+            }
+        }
+    }
+
+    select {}
 }
--- go.mod
+++ go.mod
@@ -5,11 +5,16 @@ go 1.17
 require (
 	github.com/golang-migrate/migrate/v4 v4.15.1
 	github.com/joho/godotenv v1.4.0
+	github.com/muka/go-bluetooth v0.0.0-20211122080231-b99792bbe62a
 )
 
 require (
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/godbus/dbus/v5 v5.0.4 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.0 // indirect
 	github.com/lib/pq v1.10.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	go.uber.org/atomic v1.6.0 // indirect
+	golang.org/x/sys v0.0.0-20211013075003-97ac67df715c // indirect
 )
