9c3c73a54ba21c9edc89715ead3700157943c5cb
Harald Held <harald.held@gmail.com>
2023-10-16 22:04:15 +0200

    Speed up queries by adding index to measurements table.

--- db/migrations/00002_create_measurements_table.up.sql
+++ db/migrations/00002_create_measurements_table.up.sql
@@ -1,18 +1,16 @@
 CREATE TABLE IF NOT EXISTS measurements
 (
-    time          TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    temperature   float,
-    humidity      float,
-    pressure      float,
-    accel_x       float,
-    accel_y       float,
-    accel_z       float,
+    time            TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    temperature     float,
+    humidity        float,
+    pressure        float,
+    accel_x         float,
+    accel_y         float,
+    accel_z         float,
     battery_voltage float,
-    tx_power      float,
-    movements     int,
-    ruuvitag_id   int,
+    tx_power        float,
+    movements       int,
+    ruuvitag_id     int,
 
     CONSTRAINT fk_ruuvitag FOREIGN KEY (ruuvitag_id) REFERENCES ruuvitag (id)
 );
-
--- SELECT create_hypertable('measurements', 'time');
---
+++ db/migrations/00003_create_time_index_on_measurements.up.sql
@@ -0,0 +1,1 @@
+CREATE INDEX idx_time ON measurements (time);
